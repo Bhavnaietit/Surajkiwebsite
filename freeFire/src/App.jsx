e5d65f7bfabdc4f1fc5b19674f7b0fb6fa1aab09
Bhavna chouhan <bhavnaietit@gmail.com>
2025-03-13 13:58:56 +0530

    first commit

---
+++ freeFire/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import iphn from'./assets/iphn16.jpg'
+const App = () => {
+  return (
+    <div>
+      <h1> Suraj ki website</h1>
+      <img src={iphn}></img>
+      <b>buy iPhone16</b>
+    </div>
+  )
+}
+
+export default App
